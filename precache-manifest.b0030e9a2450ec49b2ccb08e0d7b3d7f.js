223fe9d06c95d7e5883faf824cde6171b5c0069e
Ivan Shpynda <ivanshpynda@gmail.com>
2022-06-23 17:35:39 +0300

    Updates

---
+++ precache-manifest.b0030e9a2450ec49b2ccb08e0d7b3d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd67266f5fd6a97441cfa534d4b7068",
+    "url": "/ivan-shpynda/fields/index.html"
+  },
+  {
+    "revision": "cbb527d8ab62d8a26386",
+    "url": "/ivan-shpynda/fields/static/css/main.901e3b01.chunk.css"
+  },
+  {
+    "revision": "f7c27bc69edbd66972a0",
+    "url": "/ivan-shpynda/fields/static/js/2.f85e97da.chunk.js"
+  },
+  {
+    "revision": "895da70490f18ab891f620a8aa33309d",
+    "url": "/ivan-shpynda/fields/static/js/2.f85e97da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb527d8ab62d8a26386",
+    "url": "/ivan-shpynda/fields/static/js/main.b45ecff1.chunk.js"
+  },
+  {
+    "revision": "13db4b2fcf1212910220",
+    "url": "/ivan-shpynda/fields/static/js/runtime-main.f90e21d4.js"
+  }
+]);
