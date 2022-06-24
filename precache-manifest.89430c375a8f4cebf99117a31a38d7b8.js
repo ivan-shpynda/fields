72bb55782bec66cb7bc73e7d435b0b024b1d4d3c
Ivan Shpynda <ivanshpynda@gmail.com>
2022-06-24 11:23:38 +0300

    Updates

--- precache-manifest.89430c375a8f4cebf99117a31a38d7b8.js
+++ precache-manifest.89430c375a8f4cebf99117a31a38d7b8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ca2a395f1b539cca438db17218b6179e",
+    "revision": "24c1cb919b985ea478d176592e4b2313",
     "url": "/fields/index.html"
   },
   {
-    "revision": "e57b6f991106d90cfd15",
-    "url": "/fields/static/css/main.901e3b01.chunk.css"
+    "revision": "c9163932d113b4ad856e",
+    "url": "/fields/static/css/main.b40b85d4.chunk.css"
   },
   {
     "revision": "ab61d041e762245a6aa4",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fields/static/js/2.b96f1c4d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e57b6f991106d90cfd15",
+    "revision": "c9163932d113b4ad856e",
     "url": "/fields/static/js/main.fb18068c.chunk.js"
   },
   {
